89e4f9d979ddc7454593d7c8d5b9c405191c6bce
Artriai <jackson.whj@gmail.com>
2026-08-06 20:32:29 +0800

    release: prepare version 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.lyricsplus.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.4.1"
         
         resConfigs("en", "zh", "zh-rCN", "zh-rTW", "zh-rHK")
     }
